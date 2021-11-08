97b0c6d892dab6a27c6b6bf517588d264ab8d439
elonpump <85977108+elondad@users.noreply.github.com>
2021-11-08 20:46:00 +0530

    Update timeclosed.js

--- js/timeclosed.js
+++ js/timeclosed.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-  let birthday = "July 25, 2021 00:00:00",
+  let birthday = "Nov 21, 2021 00:00:00",
       countDown = new Date(birthday).getTime(),
       x = setInterval(function() {    
 
